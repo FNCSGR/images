8e7a443bcd64239c920228528ab8463cf2405816
Pia <218092639+FNCSGR@users.noreply.github.com>
2025-07-20 00:38:10 +0200

    Vivienne indexing

---
+++ Vivienne/tags.js
@@ -0,0 +1,6 @@
+const sensitiveTags = ["Tentacles"];
+const extraTagCategories = {
+    character: ["Solo", "Aliens", "Tentacles"],
+    visual: ["Armpit", "Feet", "Ass", "Tits", "Dick", "Hooker"],
+    activity: ["Handjob", "Titfuck", "Blowjob", "Anal", "Worship", "Trio"],
+    };
