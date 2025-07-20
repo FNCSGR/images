ff5f66e927658288afaa0cb063757a5665c9a43f
Pia <218092639+FNCSGR@users.noreply.github.com>
2025-07-20 12:13:53 +0200

    Update gallery.js

--- assets/gallery.js
+++ assets/gallery.js
@@ -154,7 +154,7 @@ async function initializeGallery() {
     jsonNames = manifestData.map(entry => entry.name);
 
     const tagCategories = {
-      artist: jsonNames,
+      artist: ["Rude Frog"],
     };
     
     //if (typeof extraTagCategories !== "undefined") {}
