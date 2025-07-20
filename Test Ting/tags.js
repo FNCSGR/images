16758889502182e41669a726a2d5aabddae94ffd
Pia <218092639+FNCSGR@users.noreply.github.com>
2025-07-20 11:42:14 +0200

    Test Ting

---
+++ Test Ting/tags.js
@@ -0,0 +1,7 @@
+const sensitiveTags = ["Werewolf", "Monster/Beast"];
+const extraTagCategories = {
+    character: ["Solo", "Yen", "Triss", "Anna", "Cerys", "Bea", "Ves", "Orianna", "Philippa", "Keira", "Twin", "Guards", "Succubus", "Johnny", "Werewolf", "Tentacles", 
+        "Nekker", "Drowner", "Fiend", "Monster/Beast"],
+    visual: ["Armpit", "Feet", "Ass", "Tits", "Dick", "Pussy", "Bush", "Cum", "Bath", "POV", "Older"],
+    activity: ["Handjob", "Kissing", "Blowjob", "Rimming", "Vaginal", "Anal", "Worship", "Facesitting", "Trio", "Quartet+"],
+    };
