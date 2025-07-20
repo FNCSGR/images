cd5a0b954ccbca1f087ba23bbf9ae297853e4dbc
Pia <218092639+FNCSGR@users.noreply.github.com>
2025-07-20 13:02:37 +0200

    Restore artist names

--- assets/gallery.js
+++ assets/gallery.js
@@ -156,7 +156,7 @@ async function initializeGallery() {
     jsonNames = manifestData.map(entry => entry.name);
 
     const tagCategories = {
-      artist: ["Rude Frog", "GRVTY"],
+      artist: jsonNames,
     };
     
     //if (typeof extraTagCategories !== "undefined") {}
