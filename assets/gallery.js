7d27e499c9fb774994089ea1437c9039a3e9b1a6
Pia <218092639+FNCSGR@users.noreply.github.com>
2026-01-15 19:24:35 +0100

    Update gallery.js

--- assets/gallery.js
+++ assets/gallery.js
@@ -91,7 +91,7 @@ async function fillViewport() {
   let safety = 0;
 
   while (
-    sentinel.getBoundingClientRect().top < window.innerHeight + 200 &&
+    sentinel.getBoundingClientRect().top < window.innerHeight + 500 &&
     renderIndex < filteredQueue.length &&
     safety < 10
   ) {
